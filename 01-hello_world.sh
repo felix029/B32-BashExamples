438712119a74fa14bc129e6e8054288aa686b018
Félix-O Landry <felixo2997@gmail.com>
2018-12-03 09:49:52 -0500

    Ajout de code dans le fichier 01-hello_world.sh

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+# Ceci est un commentaire
+echo "Salut"
+
+# -n = empêche le retour de ligne (Vive et Linux sont sur la même ligne)
+echo -n "Vive "
+echo "Linux"
+
+# -e permet l'utilisation des car. spéciaux (comme \n par exemple)
+echo -e "123\n123\n123\n\t123"
+
+# read est l'équiv. de cin en C++
+# Quand on met quelque chose dans une variable = pas de $
+# Quand on va CHERCHER la valeur d'une variable = $
+read -p "Vous avez quel âge? " age
+echo $age "ans! Vous paraissez plus vieux..."
