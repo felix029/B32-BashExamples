949220fdba3b1ee83ef0958ef623a86a7823357b
Félix-O Landry <felixo2997@gmail.com>
2018-12-03 10:09:27 -0500

    Added code in file 03-calculs.sh

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+#Va seulement afficher le texte 10+19
+echo 10+19
+
+#Va afficher le résultat de 10+19
+echo $((10+19))
+
+#Exemple de multiplication
+read -p "Nombre 1: " premier
+read -p "Nombre 2: " second
+
+let resultat=$premier*$second
+
+echo $resultat
