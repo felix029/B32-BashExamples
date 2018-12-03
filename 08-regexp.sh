3b0df9e7a3679d65ae4b4592d5a7730dc3c772ea
Félix-O Landry <felixo2997@gmail.com>
2018-12-03 11:10:58 -0500

    Added code to 08-regexp.sh

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+echo -n "Texte : "
+read texte
+
+# =~ indique que ce qui suivra sera une expression réguliere
+# le +$ signifie que je veux une fois ou plus le pattern [0-9] d'avant jusqua la fin $
+if [[ $texte =~ ^[0-9]+$ ]];then
+	echo "Numérique"
+fi
