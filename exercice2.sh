120ebada0d95d2acfb8f7901aa3919ceee6e26a3
Félix-O Landry <felixo2997@gmail.com>
2018-12-03 12:18:58 -0500

    Code added in exercice2.sh

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+menu() {
+
+	echo ""
+	echo "===== CVM Soft ====="
+	echo "| d = Dictionnaire |"
+	echo "| q = Quitter      |"
+	echo "--------------------"
+	echo ""
+	echo ""
+	read -p "Votre choix: " choix
+clear
+}
+
+choixFct() {
+	if test $choix = d
+	then
+		./exercice1.sh
+		sleep 2
+	fi
+clear
+}
+
+
+while [[ $choix != "q" ]]
+do
+	menu
+	choixFct
+done
