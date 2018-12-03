8d9f5c1252938a5eab6f9a6dd8c18d899e735eed
Félix-O Landry <felixo2997@gmail.com>
2018-12-03 10:51:13 -0500

    Added code in file 05-boucles.sh

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+#Deux accent graves et non des apostrophes ``
+#Ici on demande de mettre le resultat de ls dans la variable liste
+liste=`ls`
+
+#for each, pour chaque elements dans $liste, met le dans fic
+for fic in $liste
+do
+	echo $fic
+done
+
+resultat=o
+
+# -o = or
+# -a = and
+
+while test $resultat = o -o $resultat = O
+do
+	read -p "Entrer o pour répéter la boucle: " resultat
+done
