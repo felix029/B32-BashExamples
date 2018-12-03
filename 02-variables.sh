1ff98b2511745a94b33de60a448841d66aecd8e1
Félix-O Landry <felixo2997@gmail.com>
2018-12-03 10:02:28 -0500

    Code added in file 02-variables.sh

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+# IL NE DOIT PAS Y AVOIR D'ESPACE ENTRE prenom=KimCharles sinon marchera pas
+prenom=KimCharles
+echo $prenom
+
+# Forcer une déclaration d'une variable "int"
+declare -i age=21
+
+# Mettre une variable READ ONLY (comme const en C++)
+declare -r age2=18
+
+# Les variables système
+#Nom de l'executable
+echo $0
+
+#Arguments lors de l'appel, allant jusqu'a 9
+echo $1
+echo $2
+
+#Permet de savoir le nombre d'arguments
+echo $#
+
+
