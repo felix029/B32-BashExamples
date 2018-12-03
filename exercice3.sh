b06048f023cfe132f281dc2257d9de7c62298227
Félix-O Landry <felixo2997@gmail.com>
2018-12-03 12:36:57 -0500

    Code added to exercice3.sh

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+#NE FONCTIONNE PAS
+
+declare -a Space=( )
+
+for ((i=0;i<20;i++))
+do
+		for ((j=0;j<$i;j++))
+		do
+		echo -n " "
+		done
+	echo -n "@"
+	sleep 0.3
+	clear
+done
+
+echo "                    BOOM"
+sleep 0.8
+clear
